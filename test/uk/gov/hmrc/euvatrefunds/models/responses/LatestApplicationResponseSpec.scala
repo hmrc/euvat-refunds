1358682aa5d8d5e8bf80e88b3e702749bbfdc5df
Dorothea87 <147050615+Dorothea87@users.noreply.github.com>
2026-07-13 16:23:25 +0100

    DTR-5643 set submission and application status to optional value

--- test/uk/gov/hmrc/euvatrefunds/models/responses/LatestApplicationResponseSpec.scala
+++ test/uk/gov/hmrc/euvatrefunds/models/responses/LatestApplicationResponseSpec.scala
@@ -30,8 +30,8 @@ class LatestApplicationResponseSpec extends AnyWordSpec with Matchers {
     periodStartDate      = LocalDateTime.of(2025, 2, 1, 0, 0),
     periodEndDate        = LocalDateTime.of(2025, 5, 31, 23, 59),
     applicationNumber    = "GB0000000000000133",
-    applicationStatus    = "D",
-    submissionStatus     = "S",
+    applicationStatus    = Some("D"),
+    submissionStatus     = Some("S"),
     applicationVersion   = LocalDateTime.of(2025, 2, 11, 10, 38)
   )
 
--- test/uk/gov/hmrc/euvatrefunds/models/responses/LatestApplicationSpec.scala
+++ test/uk/gov/hmrc/euvatrefunds/models/responses/LatestApplicationSpec.scala
@@ -30,8 +30,8 @@ class LatestApplicationSpec extends AnyWordSpec with Matchers {
     periodStartDate      = LocalDateTime.of(2025, 2, 1, 0, 0),
     periodEndDate        = LocalDateTime.of(2025, 5, 31, 23, 59),
     applicationNumber    = "GB0000000000000133",
-    applicationStatus    = "D",
-    submissionStatus     = "S",
+    applicationStatus    = Some("D"),
+    submissionStatus     = Some("S"),
     applicationVersion   = LocalDateTime.of(2025, 2, 11, 10, 38)
   )
 
@@ -67,7 +67,7 @@ class LatestApplicationSpec extends AnyWordSpec with Matchers {
 
       model.applicationId        shouldBe 133
       model.refundingCountryCode shouldBe "LV"
-      model.applicationStatus    shouldBe "D"
+      model.applicationStatus    shouldBe Some("D")
     }
 
     "round-trip JSON (serialize then deserialize)" in {
